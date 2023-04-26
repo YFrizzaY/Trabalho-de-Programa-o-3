a81a21e7f1f09c47fee975cad806daaaaeafa49e
Paulo Henrique <104027031+YFrizzaY@users.noreply.github.com>
2023-04-26 15:13:23 -0300

    Add files via upload

---
+++ Quiz Application with Timer/js/questions.js
@@ -0,0 +1,71 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Sim, aquilo era uma banana",
+    answer: "Soraka",
+    options: [
+      "Soraka",
+      "Jayce",
+      "Lee sin",
+      "Ivern"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Lima-Oscar-Lima!",
+    answer: "Corki",
+    options: [
+      "Singed",
+      "Gragas",
+      "Corki",
+      "Ziggs"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Ok",
+    answer: "Rammus",
+    options: [
+      "Graves",
+      "Rammus",
+      "Malphite",
+      "Rengar"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Mil vidas darei por Freljord",
+    answer: "Anivia",
+    options: [
+      "Volibear",
+      "Ashe",
+      "Sejuani",
+      "Anivia"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Que sejam garras ou lanças, seu fim será o mesmo",
+    answer: "Nidalee",
+    options: [
+      "Nidalee",
+      "Pantheon",
+      "Xin Zhao",
+      "Garen"
+    ]
+  },
+  
+
+    {
+    numb: 6,
+    question: "Suas lágrimas salgarão o oceano",
+    answer: "Gangplank",
+    options: [
+      "Pyke",
+      "Gangplank",
+      "Illaoi",
+      "Miss Fortune"
+    ]
+  },
+];
